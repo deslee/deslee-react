73925f75eff64b7d8458e5d19e66d89c00da6c05
Desmond Lee <desmondclee@gmail.com>
2014-09-16 01:45:00 -0500

    bodyparser

--- server/tokenProvider.js
+++ server/tokenProvider.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var FirebaseTokenGenerator = require('firebase-token-generator');
 var secret = require('./secret');
+var bodyParser = require('body-parser');
 
 // secret.js module.exports = 'key'
 
 var app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 app.post('/', function(req,res) {
 	var tokenGenerator = new FirebaseTokenGenerator(secret.secret);
 	if (req.body.password !== secret.password) {
