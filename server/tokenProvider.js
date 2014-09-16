2fdf311e67236998738346cb3ba4a73e8c565991
Desmond Lee <desmondclee@gmail.com>
2014-09-16 01:12:01 -0500

    added token provider

--- server/tokenProvider.js
+++ server/tokenProvider.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var FirebaseTokenGenerator = require('firebase-token-generator');
+var secret = require('./secret');
+
 
 var app = express();
 app.get('/', function(req,res) {
-	var tokenGenerator = new FirebaseTokenGenerator('vrVwXBgSp9fibS1qky1AS5ZdtfiSXpZ777Lyc7sj');
+	var tokenGenerator = new FirebaseTokenGenerator(secret);
 	var token = tokenGenerator.createToken({uid: "1"}, {admin: true});
 	res.send(token);
 });
